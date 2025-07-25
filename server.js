0f859ae3f293c4432edf01bfd43de31b85f4135e
Md Mehedi Hasan <mehedihasan.cse.2002@gmail.com>
2025-07-25 23:42:54 +0600

    Add mongoose as a dependency and set up database connection in server.js

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require('express')
 const app = express();
 const expressLayouts = require('express-ejs-layouts')
+const mongoose = require('mongoose')
+
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views/')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+
+mongoose.connect(process.env.DATABASE_URL)
+
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
 
 const indexRoute = require('./routes/index')
 app.use('/', indexRoute);
