e212a364f45bd1cababf0fd350d68b791d65bc4d
Md Mehedi Hasan <mehedihasan.cse.2002@gmail.com>
2025-07-25 22:23:19 +0600

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express();
+const expressLayouts = require('express-ejs-layouts')
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views/')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+const indexRoute = require('./routes/index')
+app.use('/', indexRoute);
+
+app.listen(3000, () => {
+    console.log("Listenning port 3000");
+})
